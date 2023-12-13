82c23345ac065c14ea1849d48cc8c147ba306e0a
Julien Bodart <43612194+julbod@users.noreply.github.com>
2023-12-13 10:12:34 -0800

    Code to move all png created files from QAQC to main folder

---
+++ mv_files_newdir.py
@@ -0,0 +1,22 @@
+# This script analyses all files matching desired extension in folder
+# and moves them across to new directory. This is to facilitate processing
+# of radar data 
+from pathlib import Path
+
+# Move SEGYs
+# ----------------------------------
+var_name = 'SnowDepth'
+Path("D:/Vancouver_Island_University/Wx_station/wx_data_processing/QAQC/v2/merged_figures/" + (var_name)).mkdir(parents=True, exist_ok=True)
+
+import os, shutil
+for root, dirs, files in os.walk('D:/Vancouver_Island_University/Wx_station/wx_data_processing/QAQC/v2/individual_figures/'):
+    if var_name not in root:
+        continue
+    else:
+        for file in files:
+            if file.endswith(".png"):
+                print(os.path.join(root, file))
+    
+                # move to new directory
+                shutil.copy(os.path.abspath(root + '/' + file), 'D:/Vancouver_Island_University/Wx_station/wx_data_processing/QAQC/v2/merged_figures/' + var_name)
+# ---------------------------------

c24ada05ab1117ba5cc6089debfe3ecc5e6b37db
Julien Bodart <43612194+julbod@users.noreply.github.com>
2024-02-09 17:25:01 -0800

    Added line that deals with 'pk' if needed

--- mv_files_newdir.py
+++ mv_files_newdir.py
@@ -5,13 +5,15 @@ from pathlib import Path
 
 # Move SEGYs
 # ----------------------------------
-var_name = 'PC_Raw_Pipe'
+var_name = 'Pk_Wind_Dir'
 Path("D:/Vancouver_Island_University/Wx_station/wx_data_processing/QAQC/v2/merged_figures/" + (var_name)).mkdir(parents=True, exist_ok=True)
 
 import os, shutil
 for root, dirs, files in os.walk('D:/Vancouver_Island_University/Wx_station/wx_data_processing/QAQC/v2/individual_figures/'):
     if var_name not in root:
         continue
+    #elif 'Pk' in root: # remove 'Pk' pngs (e.g. Wind variables)
+    #    continue
     else:
         for file in files:
             if file.endswith(".png"):
